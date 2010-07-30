298d2b088e119b94ab65f8deaa44a65978b3f7a5
fabrizio <fcm@unesc.net>
2010-07-30 20:49:32 -0300

    remove welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Railstutorial::Application.routes.draw do |map|
 
   get "pages/contact"
 
-get "pages/about"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +54,7 @@ get "pages/about"
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
