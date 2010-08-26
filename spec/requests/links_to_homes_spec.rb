f88f64555281675c9437f704f745d932120fdb5a
fabrizio <fabrizio.machado@gmail.com>
2010-08-26 19:04:32 -0300

    Testando commit

---
+++ spec/requests/links_to_homes_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "LinksToHomes" do
+  describe "GET /links_to_homes" do
+    it "works! (now write some real specs)" do
+      get links_to_homes_path
+    end
+  end
+end
