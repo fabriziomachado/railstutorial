a02b56d7471d26256edec9f71b90efd3248ba038
fabrizio <fcm@unesc.net>
2010-09-14 19:37:03 -0300

    pages spec edit

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,8 +10,7 @@ describe PagesController do
    
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                  :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector('title', :content => "Ruby on Rails Tutorial Sample App | Home")
     end  
     
   end
